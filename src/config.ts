d1f34dbd3efec0b57c71d6b1dbc96faa9e5290af
KiHanahare <kihanahare@qq.com>
2024-08-10 23:55:28 +0800

    ✨ New blog: random-talk & About
    update About page and add a new post "random-talk".

--- src/config.ts
+++ src/config.ts
@@ -7,21 +7,21 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
+  title: 'Hanahare Heaven',
+  subtitle: 'Heaven of KiHanahare',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    enable: true,
+    src: 'assets/images/108906379_p0.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
+      enable: true,         // Display the credit text of the banner image
+      text: '夕阳标志着白天的谢幕@HKcutie⭐',              // Credit text to be displayed
+      url: 'https://www.pixiv.net/artworks/108906379'                // (Optional) URL link to the original artwork or artist's page
     }
   },
   favicon: [    // Leave this array empty to use the default favicon
@@ -40,33 +40,33 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/kihanahare/HanahareHeaven',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/99630657_p0.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'KiHanahare',
+  bio: 'I know that I know nothing.',
   links: [
-    {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
-    },
-    {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
+    // {
+    //   name: 'Twitter',
+    //   icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+    //                                     // You will need to install the corresponding icon set if it's not already included
+    //                                     // `pnpm add @iconify-json/<icon-set-name>`
+    //   url: 'https://twitter.com',
+    // },
+    // {
+    //   name: 'Steam',
+    //   icon: 'fa6-brands:steam',
+    //   url: 'https://store.steampowered.com',
+    // },
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/kihanahare/HanahareHeaven',
     },
   ],
 }
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,9 +1,19 @@
 # About
-This is the demo site for [Fuwari](https://github.com/saicaca/fuwari).
 
-::github{repo="saicaca/fuwari"}
+你好，我是 KiHanahare，或许你见过另一个我。
+
+成分复杂，喜欢折腾，又菜又爱玩。
+
+SWPU [NSSCTF](https://www.nssctf.cn/) 战队现役 Web & Reverse 手。
+
+喜欢分享一些杂谈，或者有趣的折腾经历。
+
+以下是 Blog 留档，基于 [Astro](https://astro.build/) 构建，使用 [Fuwari](https://github.com/saicaca/fuwari) 主题。
+
+::github{repo="kihanahare/HanahareHeaven"}
 
 > ### Sources of images used in this site
-> - [Unsplash](https://unsplash.com/)
-> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
-> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
+>
+> - [Untitled](https://www.pixiv.net/artworks/99630657) by [茗猫](https://www.pixiv.net/users/16462721)
+> - [夕阳标志着白天的谢幕](https://www.pixiv.net/artworks/108906379) by [HKcutie⭐](https://www.pixiv.net/users/54867079)
+> - [暖かい友達](https://www.pixiv.net/artworks/52793674) by [なつ](https://www.pixiv.net/users/9179925)
